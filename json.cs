7d6b0d888d5e743674538ae2a2500005afee78ee
asag2 <76063736+asag2@users.noreply.github.com>
2021-06-09 01:49:06 +0200

    Delete json.cs

--- json.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class json
-{
-    public string firstname { get; set; }
-    public string lastname { get; set; }
-    public string email { get; set; }
-    public string phone { get; set; }
-}
